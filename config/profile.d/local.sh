51747100af60e8c8dc8118cfdc5f35fd06097e6f
IMOKURI <nenegi.01mo@gmail.com>
2019-09-13 17:59:06 +0900

    update installer

--- config/profile.d/local.sh
+++ config/profile.d/local.sh
@@ -13,6 +13,7 @@ export PATH=$HOME/neovim/bin:$HOME/vim/bin:$HOME/node_modules/.bin:$HOME/.local/
 export LESS='-i -M -R -W -z-3 -x4 -F -X'
 
 export EDITOR="vi"
+export KUBE_EDITOR="/usr/bin/vi"
 
 if [ -f ~/.hosts ]; then
   export HOSTALIASES=~/.hosts
