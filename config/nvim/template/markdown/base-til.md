0e26dadc7f9ff917153550956f94c3c9a3c24b88
IMOKURI <nenegi.01mo@gmail.com>
2020-02-06 14:06:15 +0900

    add template

---
+++ config/nvim/template/markdown/base-til.md
@@ -0,0 +1,7 @@
+---
+date: 2020/MM/DD HH:00-HH:00
+title: {{_cursor_}}
+location: 
+presenter: 
+---
+
---
+++ config/nvim/template/python/snip-exclusion.py
@@ -0,0 +1,13 @@
+
+import fcntl
+import sys
+
+
+def main():
+    # 二重起動チェック
+    with open(__file__.replace('.py', '.lock'), 'w') as fp:
+        try:
+            fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except IOError:
+            sys.stderr.write('Program is already running.')
+            sys.exit(1)
