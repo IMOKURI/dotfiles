66b85cdef61391ffc1b29781425c2a4c654e0e2b
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-01-28 10:40:33 +0900

    add mapping

--- config/nvim/lua/plugin/rc/vim-table-mode.lua
+++ config/nvim/lua/plugin/rc/vim-table-mode.lua
@@ -2,6 +2,8 @@ local M = {}
 
 function M.setup()
     vim.g.table_mode_always_active = 1
+
+    vim.api.nvim_set_keymap("n", "<Leader><Leader>a", "<cmd>TableModeRealign<CR>", { noremap = true, silent = true, })
 end
 
 return M
