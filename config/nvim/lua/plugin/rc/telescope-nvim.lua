416e3552cea569987329489824137ba81f97407f
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-02-08 23:03:03 +0900

    add helper functions

--- config/nvim/lua/plugin/rc/telescope-nvim.lua
+++ config/nvim/lua/plugin/rc/telescope-nvim.lua
@@ -1,25 +1,27 @@
 local M = {}
+local h = require('helper')
+local telescope = require('telescope')
 
 function M.config()
-    vim.api.nvim_set_keymap("n", "<Leader>f", "<cmd>lua require('telescope.builtin').git_files{}<CR>",                             { noremap = true, silent = true, })
-    vim.api.nvim_set_keymap("n", "<Leader>F", "<cmd>lua require('telescope').extensions.fzf_writer.files()<CR>",                   { noremap = true, silent = true, })
-    vim.api.nvim_set_keymap("n", "<Leader>g", "<cmd>lua require('telescope').extensions.fzf_writer.grep()<CR>",                    { noremap = true, silent = true, })
-    vim.api.nvim_set_keymap("n", "<Leader>o", "<cmd>lua require('telescope').extensions.frecency.frecency()<CR>",                  { noremap = true, silent = true, })
-    vim.api.nvim_set_keymap("n", "<Leader>p", "<cmd>lua require('telescope').extensions.project.project{ change_dir = true }<CR>", { noremap = true, silent = true, })
-    vim.api.nvim_set_keymap("n", "<Leader>b", "<cmd>lua require('telescope.builtin').buffers{}<CR>",                               { noremap = true, silent = true, })
-    vim.api.nvim_set_keymap("n", "<Leader>s", "<cmd>lua require('telescope.builtin').lsp_document_symbols{}<CR>",                  { noremap = true, silent = true, })
-    vim.api.nvim_set_keymap("n", "<Leader>[", "<cmd>lua require('telescope.builtin').lsp_references{}<CR>",                        { noremap = true, silent = true, })
-    vim.api.nvim_set_keymap("n", "<Leader>t", "<cmd>lua require('telescope.builtin').treesitter{}<CR>",                            { noremap = true, silent = true, })
+    h.map("n", "<Leader>f", "<cmd>lua require('telescope.builtin').git_files{}<CR>"                             )
+    h.map("n", "<Leader>F", "<cmd>lua require('telescope').extensions.fzf_writer.files()<CR>"                   )
+    h.map("n", "<Leader>g", "<cmd>lua require('telescope').extensions.fzf_writer.grep()<CR>"                    )
+    h.map("n", "<Leader>o", "<cmd>lua require('telescope').extensions.frecency.frecency()<CR>"                  )
+    h.map("n", "<Leader>p", "<cmd>lua require('telescope').extensions.project.project{ change_dir = true }<CR>" )
+    h.map("n", "<Leader>b", "<cmd>lua require('telescope.builtin').buffers{}<CR>"                               )
+    h.map("n", "<Leader>s", "<cmd>lua require('telescope.builtin').lsp_document_symbols{}<CR>"                  )
+    h.map("n", "<Leader>[", "<cmd>lua require('telescope.builtin').lsp_references{}<CR>"                        )
+    h.map("n", "<Leader>t", "<cmd>lua require('telescope.builtin').treesitter{}<CR>"                            )
 
-    require('telescope').setup{
+    telescope.setup{
         defaults = {
             layout_strategy = "flex",
         }
     }
-    require('telescope').load_extension('project')
-    require('telescope').load_extension('frecency')
-    require('telescope').load_extension('fzf_writer')
-    require('telescope').load_extension('fzy_native')
+    telescope.load_extension('project')
+    telescope.load_extension('frecency')
+    telescope.load_extension('fzf_writer')
+    telescope.load_extension('fzy_native')
 end
 
 return M
--- config/nvim/lua/rc/netrw.lua
+++ config/nvim/lua/rc/netrw.lua
@@ -1,17 +1,19 @@
+local g = vim.g
+
 -- one file per line with timestamp information and file size
-vim.g.netrw_liststyle = 1
+g.netrw_liststyle = 1
 
 -- suppress the banner
-vim.g.netrw_banner = 0
+g.netrw_banner = 0
 
 -- human-readable
-vim.g.netrw_sizestyle = 'H'
+g.netrw_sizestyle = 'H'
 
 -- specify format string to vim's strftime()
-vim.g.netrw_timefmt = '%Y/%m/%d(%a) %H:%M:%S'
+g.netrw_timefmt = '%Y/%m/%d(%a) %H:%M:%S'
 
 -- The home directory for where bookmarks and history are saved
-vim.g.netrw_home = os.getenv("HOME")
+g.netrw_home = os.getenv("HOME")
 
 -- the settings that netrw buffers have
-vim.g.netrw_bufsettings = 'noma nomod nu rnu nowrap ro nobl'
+g.netrw_bufsettings = 'noma nomod nu rnu nowrap ro nobl'
