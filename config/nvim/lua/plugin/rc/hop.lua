eda73d1e04abd232658794aa28dd59d8a47f1a86
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-03-10 21:36:21 +0900

    add mapping

--- config/nvim/lua/plugin/rc/hop.lua
+++ config/nvim/lua/plugin/rc/hop.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.config()
     vim.api.nvim_set_keymap('n', '<Leader>h', '<cmd>lua require("hop").hint_words()<cr>', {})
+    vim.api.nvim_set_keymap('n', '<Leader>/', '<cmd>lua require("hop").hint_patterns()<cr>', {})
 end
 
 return M
