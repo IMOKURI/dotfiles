7067321bd7f750134ba0e0c739446ed623fc8a70
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-08-24 23:03:50 +0900

    add autopairs rule

--- config/nvim/lua/plugin/rc/nvim-autopairs.lua
+++ config/nvim/lua/plugin/rc/nvim-autopairs.lua
@@ -6,6 +6,20 @@ function M.config()
 
     npairs.setup({})
 
+    -- Jinja2
+    npairs.add_rules({
+        Rule("%", "%")
+            :with_pair(function (opts)
+                local pair = opts.line:sub(opts.col - 1, opts.col)
+                return vim.tbl_contains({'{}'}, pair)
+            end),
+        Rule(" ", " ")
+            :with_pair(function (opts)
+                local pair = opts.line:sub(opts.col - 2, opts.col + 1)
+                return vim.tbl_contains({'{%%}'}, pair)
+            end),
+    })
+
     require("nvim-autopairs.completion.compe").setup({
         map_cr = true, --  map <CR> on insert mode
         map_complete = true, -- it will auto insert `(` after select function or method item
