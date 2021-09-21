5559f5dec3971c854c6a9c689a44f757b480ae38
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-09-21 21:37:53 +0900

    disable builtin

--- config/nvim/lua/rc/option.lua
+++ config/nvim/lua/rc/option.lua
@@ -2,6 +2,32 @@ local g = vim.g
 local o = vim.opt
 
 -- -----------------------------------------------------------------------------
+-- Disable builtin {{{
+
+g.loaded_gzip = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
+
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
+g.loaded_vimball = 1
+g.loaded_vimballPlugin = 1
+g.loaded_2html_plugin = 1
+
+g.loaded_matchit = 1
+g.loaded_matchparen = 1
+g.loaded_logiPat = 1
+g.loaded_rrhelper = 1
+
+-- g.loaded_netrw = 1
+-- g.loaded_netrwPlugin = 1
+-- g.loaded_netrwSettings = 1
+
+-- }}}
+
+-- -----------------------------------------------------------------------------
 -- Neovim provider {{{
 
 if vim.fn.glob("~/.local/pipx/venvs/neovim-remote/bin/python") ~= "" then
