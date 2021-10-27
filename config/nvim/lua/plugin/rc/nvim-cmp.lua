8caec0ce64e8f9aca949900888104e998d878166
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-10-27 13:39:08 +0900

    cmp cmdline

--- config/nvim/lua/plugin/rc/nvim-cmp.lua
+++ config/nvim/lua/plugin/rc/nvim-cmp.lua
@@ -1,6 +1,15 @@
 local M = {}
 local cmp = require('cmp')
 
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 function M.config()
     cmp.setup {
         -- completion = {
@@ -36,16 +45,38 @@ function M.config()
         },
 
         mapping = {
-            ['<C-p>'] = cmp.mapping.select_prev_item(),
-            ['<C-n>'] = cmp.mapping.select_next_item(),
-            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.close(),
+            ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+            ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+            ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+            ['<C-e>'] = cmp.mapping({
+                i = cmp.mapping.abort(),
+                c = cmp.mapping.close(),
+            }),
             ['<CR>'] = cmp.mapping.confirm({
                 behavior = cmp.ConfirmBehavior.Insert,
                 select = true,
             }),
+
+            ["<C-n>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif vim.fn["vsnip#available"](1) == 1 then
+                    feedkey("<Plug>(vsnip-expand-or-jump)", "")
+                elseif has_words_before() then
+                    cmp.complete()
+                else
+                    fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                end
+            end, { "i", "s" }),
+
+            ["<C-p>"] = cmp.mapping(function()
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                    feedkey("<Plug>(vsnip-jump-prev)", "")
+                end
+            end, { "i", "s" }),
+
             ['<C-k>'] = function(fallback)
                 if cmp.visible() then
                     vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, true, true))
@@ -58,6 +89,7 @@ function M.config()
         },
 
         sources = {
+            { name = 'buffer' },
             { name = 'cmdline' },
             { name = 'emoji' },
             { name = 'look' },
@@ -70,6 +102,24 @@ function M.config()
             { name = 'vsnip' },
         },
     }
+end
+
+function M.cmdline_config()
+    -- Use buffer source for `/`.
+    cmp.setup.cmdline('/', {
+        sources = {
+            { name = 'buffer' }
+        }
+    })
+
+    -- Use cmdline & path source for ':'.
+    cmp.setup.cmdline(':', {
+        sources = cmp.config.sources({
+            { name = 'path' }
+        }, {
+            { name = 'cmdline' }
+        })
+    })
 end
 
 return M
