53b2da06c53f65dcfecbea9923011f36692051bd
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-11-02 10:18:12 +0900

    update lsp-installer

--- config/nvim/lua/plugin/rc/nvim-lsp-installer.lua
+++ config/nvim/lua/plugin/rc/nvim-lsp-installer.lua
@@ -15,6 +15,28 @@ local servers = {
 }
 
 function M.config()
+    local lsp_status = require("lsp-status")
+    lsp_status.register_progress()
+
+    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local capabilities = lsp_status.capabilities
+    capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+    local lspconfig = require("lspconfig")
+
+    local on_attach_vim = function(client)
+        lsp_status.on_attach(client)
+
+        vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+            vim.lsp.diagnostic.on_publish_diagnostics, {
+                virtual_text = true,
+                underline = true,
+                signs = true,
+                update_in_insert = true,
+            }
+        )
+    end
+
     local lsp_installer = require('nvim-lsp-installer')
 
     for _, name in pairs(servers) do
@@ -27,6 +49,90 @@ function M.config()
             end
         end
     end
+
+    lsp_installer.on_server_ready(function(server)
+        local opts = {}
+
+        opts.capabilities = capabilities
+        opts.on_attach = on_attach_vim
+
+        if server.name == "diagnosticls" then
+            opts.filetypes = {
+                'sh'
+            }
+            opts.init_options = {
+                formatters = {
+                    shfmt = {
+                        command = "shfmt",
+                        args = {"-i", "4", "-sr", "-ci"}
+                    }
+                },
+                formatFiletypes = {
+                    sh = "shfmt"
+                }
+            }
+        end
+
+        if server.name == "sumneko_lua" then
+            opts.settings = {
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                        path = vim.split(package.path, ';'),
+                    },
+                    diagnostics = {
+                        enable = true,
+                        globals = vim.list_extend(
+                            {
+                                "use",
+                                "vim",
+                            },
+                            {}
+                        )
+                    }
+                }
+            }
+        end
+
+        if server.name == "pyright" then
+            opts.settings = {
+                -- https://github.com/microsoft/pyright/blob/master/docs/settings.md
+                pyright = {},
+                python = {
+                    pythonPath = vim.fn.exepath("python"),
+                    analysis = {
+                        autoImportCompletions = true,
+                        autoSearchPaths = true,
+                        diagnosticMode = 'workspace',
+                        typeCheckingMode = 'basic',
+                        useLibraryCodeForTypes = true
+                    }
+                }
+            }
+        end
+
+        if server.name == "yamlls" then
+            opts.settings = {
+                yaml = {
+                    format = {
+                        enable = true,
+                        printWidth = 120,
+                    },
+                    validate = true,
+                    hover = true,
+                    completion = true,
+                    schemas = {
+                        kubernetes = "/*"
+                    }
+                }
+            }
+        end
+
+        -- This setup() function is exactly the same as lspconfig's setup function.
+        -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/ADVANCED_README.md
+        server:setup(opts)
+    end)
+
 end
 
 return M
