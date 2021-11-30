18f1c977d3212fed61b09aefb422e336f15cac3e
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-11-30 15:12:24 +0900

    update disable default plugins

--- config/nvim/lua/rc/option.lua
+++ config/nvim/lua/rc/option.lua
@@ -4,22 +4,25 @@ local o = vim.opt
 -- -----------------------------------------------------------------------------
 -- Disable builtin {{{
 
-g.loaded_gzip = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
-
-g.loaded_getscript = 1
-g.loaded_getscriptPlugin = 1
-g.loaded_vimball = 1
-g.loaded_vimballPlugin = 1
-g.loaded_2html_plugin = 1
-
-g.loaded_matchit = 1
-g.loaded_matchparen = 1
-g.loaded_logiPat = 1
-g.loaded_rrhelper = 1
+g.did_install_default_menus = 1
+g.did_install_syntax_menu   = 1
+g.loaded_2html_plugin       = 1
+g.loaded_getscript          = 1
+g.loaded_getscriptPlugin    = 1
+g.loaded_gzip               = 1
+g.loaded_logiPat            = 1
+g.loaded_man                = 1
+g.loaded_matchit            = 1
+g.loaded_matchparen         = 1
+g.loaded_rrhelper           = 1
+g.loaded_tar                = 1
+g.loaded_tarPlugin          = 1
+g.loaded_tutor_mode_plugin  = 1
+g.loaded_vimball            = 1
+g.loaded_vimballPlugin      = 1
+g.loaded_zip                = 1
+g.loaded_zipPlugin          = 1
+g.skip_loading_mswin        = 1
 
 -- g.loaded_netrw = 1
 -- g.loaded_netrwPlugin = 1
