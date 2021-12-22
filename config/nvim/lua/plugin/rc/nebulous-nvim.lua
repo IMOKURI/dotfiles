dfc63568dd78f1305fdc5145a8c72f09c9393a5d
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-12-22 12:29:13 +0900

    update color

--- config/nvim/lua/plugin/rc/nebulous-nvim.lua
+++ config/nvim/lua/plugin/rc/nebulous-nvim.lua
@@ -1,8 +1,14 @@
 local M = {}
+local colors = require("nebulous.functions").get_colors("midnight")
 
 function M.config()
     require('nebulous').setup({
         variant = "midnight",
+        custom_colors = {
+            CursorLineNr = { fg = colors.Aqua },
+            HighlightedLineNr = { fg = colors.White },
+            DimLineNr = { fg = colors.DarkGrey },
+        }
     })
 end
 
