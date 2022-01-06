279bacf7e812b47788be65becf819ac7f74dedf5
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-01-06 20:07:06 +0900

    add autopairs rule

--- config/nvim/lua/plugin/rc/nvim-autopairs.lua
+++ config/nvim/lua/plugin/rc/nvim-autopairs.lua
@@ -21,6 +21,33 @@ function M.config()
                 return vim.tbl_contains({'{%%}'}, pair)
             end),
     })
+
+    npairs.add_rules {
+        Rule(' ', ' ')
+            :with_pair(function (opts)
+                local pair = opts.line:sub(opts.col - 1, opts.col)
+                return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+            end),
+        Rule('( ', ' )')
+            :with_pair(function() return false end)
+            :with_move(function(opts)
+                return opts.prev_char:match('.%)') ~= nil
+            end)
+            :use_key(')'),
+        Rule('{ ', ' }')
+            :with_pair(function() return false end)
+            :with_move(function(opts)
+                return opts.prev_char:match('.%}') ~= nil
+            end)
+            :use_key('}'),
+        Rule('[ ', ' ]')
+            :with_pair(function() return false end)
+            :with_move(function(opts)
+                return opts.prev_char:match('.%]') ~= nil
+            end)
+            :use_key(']')
+    }
+
 end
 
 return M
