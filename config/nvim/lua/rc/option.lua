c4862a59364221ee81f9c57d8309599c3faa3004
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-01-30 20:24:31 +0900

    remove python2 config

--- config/nvim/lua/rc/option.lua
+++ config/nvim/lua/rc/option.lua
@@ -38,7 +38,6 @@ if vim.fn.glob("~/.local/pipx/venvs/neovim-remote/bin/python") ~= "" then
 end
 g.loaded_node_provider = 0
 g.loaded_perl_provider = 0
-g.loaded_python_provider = 0
 g.loaded_ruby_provider = 0
 
 -- }}}
