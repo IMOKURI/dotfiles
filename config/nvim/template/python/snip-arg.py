c67a6a874b5232edac832a8e61741c88babab8e8
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-02-17 13:40:20 +0900

    update template

--- config/nvim/template/python/snip-arg.py
+++ config/nvim/template/python/snip-arg.py
@@ -12,28 +12,10 @@ def get_args():
     This is great script!
     """)
 
-    parser.add_argument('--single-args',
-                        nargs=1,
-                        required=True,
-                        help='Help message.')
-
-    parser.add_argument('--multiple-args',
-                        nargs='+',
-                        required=True,
-                        help='Help message.')
-
-    parser.add_argument('--optional-single-args',
-                        nargs='?',
-                        default='default value',
-                        help='Help message.')
-
-    parser.add_argument('--optional-multiple-args',
-                        nargs='*',
-                        default=[1, 2, 3],
-                        help='Help message.')
-
-    parser.add_argument('--enable-flag',
-                        action='store_true',
-                        help='Help message.')
+    parser.add_argument('--single-args', nargs=1, required=True, help='Help message.')
+    parser.add_argument('--multiple-args', nargs='+', required=True, help='Help message.')
+    parser.add_argument('--optional-single-args', nargs='?', default='default value', help='Help message.')
+    parser.add_argument('--optional-multiple-args', nargs='*', default=[1, 2, 3], help='Help message.')
+    parser.add_argument('--enable-flag', action='store_true', help='Help message.')
 
     return parser.parse_args()
