56a0dd43c9c4af0960fddc511a14e1218690b374
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-03-01 19:10:28 +0900

    update native autocmd settings

--- config/nvim/lua/rc/autocmd.lua
+++ config/nvim/lua/rc/autocmd.lua
@@ -1,46 +1,39 @@
 local u = require("util")
 local group_name = "MyACGroup"
 
--- @param {boolean} clear - optional, defaults to true
--- @param {string} name - autogroup name
-vim.api.nvim_create_augroup({ name = group_name, clear = true })
+vim.api.nvim_create_augroup(group_name, {})
 
--- @param {string} name - augroup name
--- @param {string | table} event - event or events to match against
--- @param {string | table} pattern - pattern or patterns to match against
--- @param {string | function} callback - function or string to execute on autocmd
--- @param {string} command - optional, vimscript command Eg. command = "let g:value_set = v:true"
--- @param {boolean} once - optional, defaults to false
 vim.api.nvim_create_autocmd({
-    event = "TextYankPost",
+    "TextYankPost",
+}, {
     group = group_name,
-    pattern = "*",
     callback = function()
         vim.highlight.on_yank()
     end,
 })
 vim.api.nvim_create_autocmd({
-    event = "BufReadPost",
+    "BufReadPost",
+}, {
     group = group_name,
-    pattern = "*",
     command = [[ if line("'\"") > 0 && line ("'\"") <= line("$") | exe "normal! g'\"" | endif ]],
 })
 vim.api.nvim_create_autocmd({
-    event = "BufWinEnter",
+    "BufWinEnter",
+}, {
     group = group_name,
-    pattern = "*",
     command = [[ if empty(&buftype) && line('.') > winheight(0) / 2 | execute 'normal! zz' | endif ]],
 })
 vim.api.nvim_create_autocmd({
-    event = "BufWritePre",
+    "BufWritePre",
+}, {
     group = group_name,
-    pattern = "*",
     callback = function()
         u.auto_mkdir(vim.fn.expand("<afile>:p:h:s?suda://??"), vim.api.nvim_eval("v:cmdbang"))
     end,
 })
 vim.api.nvim_create_autocmd({
-    event = "BufWritePost",
+    "BufWritePost",
+}, {
     group = group_name,
     pattern = "plugin.all.lua",
     command = "source <afile> | PackerCompile",
