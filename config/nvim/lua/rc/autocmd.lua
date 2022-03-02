43cec81476c11264113b64cd0fb97c7d4c2ac46f
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-03-02 19:58:10 +0900

    update autocmd

--- config/nvim/lua/rc/autocmd.lua
+++ config/nvim/lua/rc/autocmd.lua
@@ -3,37 +3,27 @@ local group_name = "MyACGroup"
 
 vim.api.nvim_create_augroup(group_name, {})
 
-vim.api.nvim_create_autocmd({
-    "TextYankPost",
-}, {
+vim.api.nvim_create_autocmd("TextYankPost", {
     group = group_name,
     callback = function()
         vim.highlight.on_yank()
     end,
 })
-vim.api.nvim_create_autocmd({
-    "BufReadPost",
-}, {
+vim.api.nvim_create_autocmd("BufReadPost", {
     group = group_name,
     command = [[ if line("'\"") > 0 && line ("'\"") <= line("$") | exe "normal! g'\"" | endif ]],
 })
-vim.api.nvim_create_autocmd({
-    "BufWinEnter",
-}, {
+vim.api.nvim_create_autocmd("BufWinEnter", {
     group = group_name,
     command = [[ if empty(&buftype) && line('.') > winheight(0) / 2 | execute 'normal! zz' | endif ]],
 })
-vim.api.nvim_create_autocmd({
-    "BufWritePre",
-}, {
+vim.api.nvim_create_autocmd("BufWritePre", {
     group = group_name,
     callback = function()
         u.auto_mkdir(vim.fn.expand("<afile>:p:h:s?suda://??"), vim.api.nvim_eval("v:cmdbang"))
     end,
 })
-vim.api.nvim_create_autocmd({
-    "BufWritePost",
-}, {
+vim.api.nvim_create_autocmd("BufWritePost", {
     group = group_name,
     pattern = "plugin.all.lua",
     command = "source <afile> | PackerCompile",
