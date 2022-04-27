91ea3c622a02493f8b1f067773d44bb878f359ed
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-04-27 16:34:00 +0900

    add border for lsp hover

--- config/nvim/lua/plugin/rc/nvim-lsp-installer.lua
+++ config/nvim/lua/plugin/rc/nvim-lsp-installer.lua
@@ -25,6 +25,11 @@ function M.config()
     local on_attach_vim = function(client)
         lsp_status.on_attach(client)
 
+        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
+        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+            vim.lsp.handlers.signature_help,
+            { border = "single" }
+        )
         vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
             virtual_text = {
                 source = true,
