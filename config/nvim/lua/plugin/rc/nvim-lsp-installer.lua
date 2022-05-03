ca053c7539fb9e1702e052d9bb60700703dd5909
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-05-03 12:25:52 +0900

    remove shellcheck

--- config/nvim/lua/plugin/rc/nvim-lsp-installer.lua
+++ config/nvim/lua/plugin/rc/nvim-lsp-installer.lua
@@ -72,24 +72,6 @@ function M.config()
             "yaml",
         },
         init_options = {
-            linters = {
-                shellcheck = {
-                    command = "shellcheck",
-                    debounce = 100,
-                    args = { "--format=gcc", "-" },
-                    offsetLine = 0,
-                    offsetColumn = 0,
-                    formatLines = 1,
-                    formatPattern = {
-                        "^[^:]+:(\\d+):(\\d+):\\s+([^:]+):\\s+(.*)$",
-                        { line = 1, column = 2, message = 4, security = 3 },
-                    },
-                    securities = { error = "error", warning = "warning", note = "info" },
-                },
-            },
-            filetypes = {
-                sh = "shellcheck",
-            },
             formatters = {
                 shfmt = {
                     command = "shfmt",
