beeafe743b810829ef9803b9a6dfdf56dbd5e54d
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-05-13 09:38:02 +0900

    add telescope smart history

--- config/nvim/lua/plugin/rc/telescope-nvim.lua
+++ config/nvim/lua/plugin/rc/telescope-nvim.lua
@@ -25,31 +25,20 @@ function M.config()
                     ["<Tab>"] = action_layout.toggle_preview,
                 },
             },
+            history = {
+                path = string.format("%s/telescope_history.sqlite3", vim.fn.stdpath("data")),
+                limit = 100,
+            },
         },
         extensions = {
             ["ui-select"] = {
-                require("telescope.themes").get_dropdown({
-                    -- even more opts
-                }),
-
-                -- pseudo code / specification for writing custom displays, like the one
-                -- for "codeactions"
-                -- specific_opts = {
-                --   [kind] = {
-                --     make_indexed = function(items) -> indexed_items, width,
-                --     make_displayer = function(widths) -> displayer
-                --     make_display = function(displayer) -> function(e)
-                --     make_ordinal = function(e) -> string
-                --   },
-                --   -- for example to disable the custom builtin "codeactions" display
-                --      do the following
-                --   codeactions = false,
-                -- }
+                require("telescope.themes").get_dropdown({}),
             },
         },
     })
 
     telescope.load_extension("ui-select")
+    telescope.load_extension("smart_history")
 end
 
 function M.project_files()
