5a68518047c3e1418df8a1cc83a791d369b0aec4
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-05-17 09:26:16 +0900

    refactor: use more nvim_cmd

--- config/nvim/lua/ftplugin/gitcommit.lua
+++ config/nvim/lua/ftplugin/gitcommit.lua
@@ -3,9 +3,19 @@ local M = {}
 function M.select_type()
     local _, _, type = string.find(vim.api.nvim_get_current_line(), "# (%w+) ")
 
-    vim.cmd('silent! normal! "_dip')
+    vim.api.nvim_cmd({
+        cmd = "normal",
+        args = { '"_dip' },
+        bang = true,
+        mods = {
+            emsg_silent = true,
+        },
+    }, {})
     vim.api.nvim_set_current_line(string.format("%s: ", type))
-    vim.cmd("startinsert!")
+    vim.api.nvim_cmd({
+        cmd = "startinsert",
+        bang = true,
+    }, {})
 end
 
 return M
