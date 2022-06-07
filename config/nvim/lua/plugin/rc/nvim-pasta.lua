7f2b5f3050b242b94d67428343779b7aff68c5f7
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-06-07 12:11:00 +0900

    feat: Update nvim-pasta settings.

--- config/nvim/lua/plugin/rc/nvim-pasta.lua
+++ config/nvim/lua/plugin/rc/nvim-pasta.lua
@@ -4,7 +4,10 @@ function M.config()
     vim.keymap.set("n", "p", require("pasta.mappings").p)
     vim.keymap.set("n", "P", require("pasta.mappings").P)
 
-    require("pasta").setup({})
+    require("pasta").setup({
+        -- Disable to adjust indent.
+        converters = {},
+    })
 end
 
 return M
