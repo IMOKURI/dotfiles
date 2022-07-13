2df22cad7c0dd66fdd64ef472a3839e1b47b7cbf
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-07-13 10:15:34 +0900

    feat: Update colorschema settings.
    Signed-off-by: Yoshio Sugiyama <nenegi.01mo@gmail.com>

--- config/nvim/lua/plugin/rc/catppuccin.lua
+++ config/nvim/lua/plugin/rc/catppuccin.lua
@@ -1,27 +1,27 @@
 local M = {}
 local catppuccin = require("catppuccin")
-local colors = require("catppuccin.api.colors").get_colors()
+local colors = require("catppuccin.core.palettes.init").get_palette()
 
 function M.config()
-    catppuccin.setup({})
+    catppuccin.setup({
+        custom_highlights = {
+            HighlightedCursorLineNr = { fg = colors.red },
+            HighlightedLineNr = { fg = colors.text },
+            HighlightedLineNr1 = { fg = colors.maroon },
+            HighlightedLineNr2 = { fg = colors.peach },
+            HighlightedLineNr3 = { fg = colors.yellow },
+            HighlightedLineNr4 = { fg = colors.green },
+            HighlightedLineNr5 = { fg = colors.teal },
+            DimLineNr = { fg = colors.surface1 },
 
-    catppuccin.remap({
-        HighlightedCursorLineNr = { fg = colors.red },
-        HighlightedLineNr = { fg = colors.text },
-        HighlightedLineNr1 = { fg = colors.maroon },
-        HighlightedLineNr2 = { fg = colors.peach },
-        HighlightedLineNr3 = { fg = colors.yellow },
-        HighlightedLineNr4 = { fg = colors.green },
-        HighlightedLineNr5 = { fg = colors.teal },
-        DimLineNr = { fg = colors.surface1 },
+            ActiveWindow = { bg = colors.base },
+            InactiveWindow = { bg = colors.mantle },
 
-        ActiveWindow = { bg = colors.base },
-        InactiveWindow = { bg = colors.mantle },
+            WinBarContent = { fg = colors.base, bg = colors.sapphire },
+            WinBarSeparator = { fg = colors.sapphire },
 
-        WinBarContent = { fg = colors.base, bg = colors.sapphire },
-        WinBarSeparator = { fg = colors.sapphire },
-
-        ColorColumn = { bg = colors.mantle }
+            ColorColumn = { bg = colors.mantle },
+        },
     })
 
     vim.opt.winhighlight = "Normal:ActiveWindow,NormalNC:InactiveWindow,CursorLineNr:HighlightedCursorLineNr"
