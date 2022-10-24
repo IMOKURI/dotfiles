c20346f0c228cf704ac54cae8c5cedc99568a7b2
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-10-24 16:05:12 +0900

    feat: Update gitsign mappings.
    Signed-off-by: Yoshio Sugiyama <nenegi.01mo@gmail.com>

--- config/nvim/lua/plugin/rc/gitsigns.lua
+++ config/nvim/lua/plugin/rc/gitsigns.lua
@@ -2,8 +2,35 @@ local M = {}
 
 function M.config()
     require("gitsigns").setup({
-        word_diff = false,
-        keymaps = {},
+        on_attach = function(bufnr)
+            local gs = package.loaded.gitsigns
+
+            local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+            end
+
+            -- Navigation
+            map("n", "]c", function()
+                if vim.wo.diff then
+                    return "]c"
+                end
+                vim.schedule(function() gs.next_hunk() end)
+                return "<Ignore>"
+            end, { expr = true })
+
+            map("n", "[c", function()
+                if vim.wo.diff then
+                    return "[c"
+                end
+                vim.schedule(function() gs.prev_hunk() end)
+                return "<Ignore>"
+            end, { expr = true })
+
+            -- Text object
+            map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+        end,
     })
 end
 
