78b39b72b0977ba231273559a0e394f0b6045f11
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-10-28 21:23:18 +0900

    feat: Update bash template.
    Signed-off-by: Yoshio Sugiyama <nenegi.01mo@gmail.com>

--- config/nvim/template/sh/base-main.sh
+++ config/nvim/template/sh/base-main.sh
@@ -1,8 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# https://sharats.me/posts/shell-script-best-practices/
 
-set -eu -o pipefail
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./script.sh arg-one arg-two
+
+This is an awesome bash script to make your life better.
+
+'
+    exit
+fi
+
+cd "$(dirname "$0")"
 
 #=====================================================
 # Main
 #=====================================================
+main() {
+    echo "do awesome stuff"
+}
+
+main "$@"
+
 {{_cursor_}}
