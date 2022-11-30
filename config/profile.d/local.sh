3d2e9144660de8f5779fbafcbb3ad1324ebaaf6f
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-11-30 22:18:58 +0900

    feat: Update bashrc.
    Signed-off-by: Yoshio Sugiyama <nenegi.01mo@gmail.com>

--- config/profile.d/local.sh
+++ config/profile.d/local.sh
@@ -10,21 +10,11 @@ fi
 
 export LANG="en_US.UTF-8"
 
-export PATH=$HOME/.local/bin:$HOME/bin:$PATH
-# Go
-export PATH=$HOME/go/bin:$PATH
-# Python
-export PATH=$HOME/.poetry/bin:$PATH
-# Rust
-export PATH=$PATH:/usr/lib/cargo/bin
-
 export GOPATH=$HOME/go
 
 export LESS="-i -M -R -W -z-3 -x4 -F -X"
 
 export EDITOR="nvim"
-
-# export MANPAGER='nvim +Man!'
 
 if [ -f ~/.hosts ]; then
   export HOSTALIASES=~/.hosts
