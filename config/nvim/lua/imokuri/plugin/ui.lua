f7852ae92d99fd973ac005a105e9d90e015e95a3
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2023-01-09 19:55:46 +0900

    feat: update noice settings.
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- config/nvim/lua/imokuri/plugin/ui.lua
+++ config/nvim/lua/imokuri/plugin/ui.lua
@@ -80,41 +80,15 @@ return {
         event = "VimEnter",
         config = function()
             require("noice").setup({
+                presets = {
+                    long_message_to_split = true,
+                    inc_rename = true,
+                    lsp_doc_border = true,
+                },
                 routes = {
                     {
-                        -- 20行以上の出力は split 画面で表示する
-                        view = "split",
-                        filter = { event = "msg_show", min_height = 20 },
-                    },
-                    {
-                        -- マクロ記録開始を表示する
                         view = "notify",
                         filter = { event = "msg_showmode" },
-                    },
-                    {
-                        view = "mini",
-                        filter = {
-                            event = "msg_show",
-                            kind = "",
-                            any = {
-                                { find = "fewer lines" }, -- 複数行削除
-                                { find = "more lines" }, -- 複数行追加
-                                { find = "written" }, -- ファイル保存
-                                { find = "yanked" }, -- コピー
-                            },
-                        },
-                    },
-                },
-                lsp = {
-                    hover = {
-                        opts = {
-                            border = "single",
-                        },
-                    },
-                    signature = {
-                        opts = {
-                            border = "single",
-                        },
                     },
                 },
             })
