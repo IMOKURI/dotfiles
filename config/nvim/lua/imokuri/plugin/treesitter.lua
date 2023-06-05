6a750784e9bc00f56f22f2c3f9baf5b683704c42
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2023-06-05 20:42:08 +0900

    feat: add incremental_selection mapping.
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- config/nvim/lua/imokuri/plugin/treesitter.lua
+++ config/nvim/lua/imokuri/plugin/treesitter.lua
@@ -34,9 +34,15 @@ return {
                         enable = false,
                     },
                 },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<Enter>",
+                        node_incremental = "<Enter>",
+                        node_decremental = "<BS>",
+                    },
+                },
             })
-
         end,
     },
-
 }
