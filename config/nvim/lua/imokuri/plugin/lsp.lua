26ea7f9923b21decc942599580443453dd85c59b
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2023-11-07 10:43:04 +0900

    feat: update diagnostic emoji
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- config/nvim/lua/imokuri/plugin/lsp.lua
+++ config/nvim/lua/imokuri/plugin/lsp.lua
@@ -58,7 +58,7 @@ return {
                 vim.keymap.set("n", "<Leader>k", "<Cmd>lua vim.diagnostic.goto_prev()<CR>", { buffer = bufnr })
                 vim.keymap.set("n", "<Leader>r", ":IncRename ", { buffer = bufnr }) -- depends on inc-rename.nvim
 
-                local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+                local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
                 for type, icon in pairs(signs) do
                     local hl = "DiagnosticSign" .. type
                     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
