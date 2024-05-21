0e27ec5981ed9fe8e3293fa89e0072bc04238e02
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2024-05-21 10:26:59 +0900

    feat: add tiny-devicons-auto-colors
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- config/nvim/lua/imokuri/plugin/ui.lua
+++ config/nvim/lua/imokuri/plugin/ui.lua
@@ -64,6 +64,17 @@ return {
         end,
     },
 
+    {
+        "rachartier/tiny-devicons-auto-colors.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        event = "VeryLazy",
+        config = function()
+            require('tiny-devicons-auto-colors').setup()
+        end
+    },
+
     -- Status line
     {
         "nvim-lualine/lualine.nvim",
