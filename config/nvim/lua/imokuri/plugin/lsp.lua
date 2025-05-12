5868c01e174496e4364a2db13bfc0c9e7415fa08
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2025-05-13 08:52:28 +0900

    refactor: update astro ls config
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- config/nvim/lua/imokuri/plugin/lsp.lua
+++ config/nvim/lua/imokuri/plugin/lsp.lua
@@ -85,7 +85,7 @@ return {
                 init_options = {
                     typescript = {
                         tsdk = vim.fn.expand(
-                            "~/.local/share/mise/installs/npm-astrojs-language-server/2.15.4/node_modules/typescript/lib"
+                            "~/.local/share/mise/installs/npm-astrojs-language-server/latest/node_modules/typescript/lib"
                         ),
                     },
                 },
