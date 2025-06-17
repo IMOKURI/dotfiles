b3a7eac90d079fb9118b73e59da47f04c6aed2ba
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2025-06-17 12:09:43 +0900

    feat: add floaterm
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- config/nvim/lua/imokuri/plugin/terminal.lua
+++ config/nvim/lua/imokuri/plugin/terminal.lua
@@ -1,18 +1,14 @@
 return {
     {
-        "kassio/neoterm",
+        "nvzone/floaterm",
+        dependencies = "nvzone/volt",
         keys = {
-            { "<C-t>", "<Cmd>Ttoggle<CR>", mode = "n" },
-            { "<C-t>", "<C-\\><C-n><Cmd>Ttoggle<CR>", mode = "t" },
-            { "<C-g>", "<Cmd>Topen<CR><C-w>bi", mode = "n" },
-            { "<C-g>", "<C-\\><C-n><C-w>p", mode = "t" },
+            { "<C-t>", "<Cmd>FloatermToggle<CR>", mode = "n" },
+            { "<C-t>", "<C-\\><C-n><Cmd>FloatermToggle<CR>", mode = "t" },
         },
-        config = function()
-            vim.g.neoterm_size = 20
-            vim.g.neoterm_autoscroll = 1
-            vim.g.neoterm_default_mod = "botright"
-            vim.g.neoterm_repl_python = "python"
-            vim.g.neoterm_automap_keys = ""
-        end,
+        opts = {
+            border = true,
+            size = { h = 60, w = 80 },
+        },
     },
 }
